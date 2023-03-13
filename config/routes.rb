a3d759d1c1d2d0617177bf56297b532d3f146a7d
gael carayon <gaelcarayon@hotmail.com>
2023-03-13 13:12:45 +0100

    resolve 2 but test kaputt

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'pages#home'
+  get "answer", to: "questions#answer"
+  get "ask", to: "questions#ask"
 end
